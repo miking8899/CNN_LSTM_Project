7a355d2bd3add9793db515db93740d8cc66d79de
王金標 <144295277+miking8899@users.noreply.github.com>
2023-10-26 09:23:15 +0800

    20231026上課上午

---
+++ 20231026_UBike/index.py
@@ -0,0 +1,20 @@
+import tkinter as tk 
+from tkinter import ttk 
+
+
+class Window(tk.Tk):
+    
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+    pass
+
+
+def main():
+    window=Window()
+    
+    pass
+
+
+if __name__=='main':
+    main()
+    
